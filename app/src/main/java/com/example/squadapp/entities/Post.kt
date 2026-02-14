d7ad35d1692ba1290469c5d52c08bf26b0ba8a67
Guy Barry <guybarry5@gmail.com>
2026-02-14 16:18:08 +0200

    add serialization and deserialization to Post and User entities

--- app/src/main/java/com/example/squadapp/entities/Post.kt
+++ app/src/main/java/com/example/squadapp/entities/Post.kt
@@ -8,23 +8,41 @@ data class Post(
     val user: User,
     val description: String,
     val creationTime: Date
-){
+) {
 
-companion object {
-    fun fromJson(json: Map<String, Any?>): Post {
-        val id = json["id"] as String
-        val image = json["image"] as Int
-        val description = json["description"] as String
-        val creationTime = json["creationTime"] as Date
-        val user = json["user"] as User
+    companion object {
+        // Post JSON keys
+        const val POST_ID = "id"
+        const val POST_IMAGE = "image"
+        const val POST_USER = "user"
+        const val POST_DESCRIPTION = "description"
+        const val POST_CREATION_TIME = "creationTime"
 
-        return Post(
-            id = id,
-            image = image,
-            description = description,
-            creationTime = creationTime,
-            user = user
-        )
+        fun deserialize(json: Map<String, Any?>): Post {
+            val id = json["id"] as String
+            val image = json["image"] as Int
+            val description = json["description"] as String
+            val creationTime = json["creationTime"] as Date
+            val user = json["user"] as User
+
+            return Post(
+                id = id,
+                image = image,
+                description = description,
+                creationTime = creationTime,
+                user = user
+            )
+        }
+
+        fun serialize(post: Post): Map<String, Any?> {
+            return hashMapOf(
+                POST_ID to post.id,
+                POST_IMAGE to post.image,
+                POST_USER to post.user.id.toString(), // Store user ID as foreign key reference
+                POST_DESCRIPTION to post.description,
+                POST_CREATION_TIME to post.creationTime
+            )
+        }
+
     }
-}
 }
--- app/src/main/java/com/example/squadapp/entities/User.kt
+++ app/src/main/java/com/example/squadapp/entities/User.kt
@@ -6,5 +6,32 @@ data class User(
     val username: String,
     val password: String,
     val discordTag: String
-)
+) {
+    companion object {
+        const val USER_ID = "id"
+        const val USER_PROFILE_IMAGE = "profileImage"
+        const val USER_USERNAME = "username"
+        const val USER_PASSWORD = "password"
+        const val USER_DISCORD_TAG = "discordTag"
 
+        fun deserializeUser(data: Map<String, Any?>): User {
+            return User(
+                id = (data[USER_ID] as? Long)?.toInt() ?: (data[USER_ID] as? Int) ?: 0,
+                profileImage = (data[USER_PROFILE_IMAGE] as? Long)?.toInt() ?: (data[USER_PROFILE_IMAGE] as? Int) ?: 0,
+                username = data[USER_USERNAME] as? String ?: "",
+                password = data[USER_PASSWORD] as? String ?: "",
+                discordTag = data[USER_DISCORD_TAG] as? String ?: ""
+            )
+        }
+
+        fun serializeUser(user: User): Map<String, Any?> {
+            return hashMapOf(
+                USER_ID to user.id,
+                USER_PROFILE_IMAGE to user.profileImage,
+                USER_USERNAME to user.username,
+                USER_PASSWORD to user.password,
+                USER_DISCORD_TAG to user.discordTag
+            )
+        }
+    }
+}
